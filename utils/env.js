50ff9d313cf3659ec3289b5b620d2a6a7633d8d2
Artemiss1509 <stanza-grout54@icloud.com>
2025-12-13 21:11:35 +0530

    Implement media share

--- utils/env.js
+++ utils/env.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 
 config({path: `.env.${process.env.NODE_ENV || 'dev'}.local`});
 
-export const {JWT_SECRET, JWT_EXPIRY} = process.env;
+export const {JWT_SECRET, JWT_EXPIRY, BUCKET_NAME, IAM_KEY, IAM_SECRET} = process.env;
